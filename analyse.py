0e90b044ea94755c1e8b4f045c42065ad543d7ce
jerev7 <verdet.jeremie@gmail.com>
2021-03-28 12:37:58 +0200

    moved google key to a config file

--- analyse.py
+++ analyse.py
@@ -1,86 +1,21 @@
 import pandas as pd
 import requests
+from config import KEY # importing google API key from config.py
 
-
+# Reading the file to analyse
 activities = pd.read_csv("equipment_activities.csv")
 installations = pd.read_csv("equipment_installations.csv")
-
-# print(activities[["ComInsee", "ActLib"]])
-
-
-# iterate all rows
-
-# for index, row in activities.iterrows():
-#     print(index, row['ActLib'])
-
-
-
-# Get specific row, column
-
-# print(activities.iloc[1, 2])
-
-
-
-# Get all row which match a specific value
-
-# print(activities.loc[activities["EquipementId"] == 73821])
-
-
-
-# Sort
-
-# print(activities.sort_values('ActLib', ascending=False))
-
-
-# Add column
-
-# activities["new column"] = "New"
-# print(activities.head(1))
-
-
-
-# Drop column
-
-# activities = activities.drop(columns=["new column"])
-# print(activities.head(1))
-
-install_equip = installations.iloc[0, 4]
-# print(install_equip)
-
-# rowIneed = activities.loc[activities["EquipementId"] == install_equip]
-# rowIneed["ActLib"] = installations.iloc[0, 4]
-# print(rowIneed)
-install_equip_all = installations.loc[4]
-# print(install_equip_all)
-
-# print(columns_to_add[0])
-# for column in installations.columns:
-#     if column not in activities.columns:
-#         activities[column] = installations.loc[installations["EquipementId"] == 73821, column]
-# else:
-#     print("Column already exists")
-# print(activities.columns)
-
 
 # Creating a list of all equipments
 equip_id_list = []
 equipements = installations["EquipementId"]
 for equip in equipements:
     equip_id_list.append(equip)
-# print(equip_id_list)
-
-# all_rows_of_one_equip = activities.loc[activities["EquipementId"] == 73821]
-# print(all_rows_of_one_equip)
 
 # We delete columns existing in both csv
 for column in installations.columns:
     if column in activities.columns and column != "EquipementId":
         activities = activities.drop(columns=column)
-
-# print(equip_id_list)
-# activities = activities.drop(columns=["ComInsee", "ComLib", "EquNbEquIdentique"])
-# activities = activities.drop(columns=["ComLib"])
-# activities = activities.drop(columns=["EquNbEquIdentique"])
 
 # We merge both csv together using common key EquipementId
 merged_data = pd.merge(installations, activities, how="left", on="EquipementId")
@@ -88,19 +23,20 @@ merged_data = pd.merge(installations, activities, how="left", on="EquipementId")
 
 def address_from_latitude_longitude(latitude, longitude):
     """
-    Function to get the real address using latitude longitude.
+    Function using google geocode API to get the real address using latitude longitude.
     """
-    
 
     try:
-        r = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={key}")
+        r = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={KEY}")
         address = r.json()["results"][0]["formatted_address"]
         print("Google request was successful !")
     except:
         address = "Address not found"
+        print("Google request failed")
 
     return address.replace(",", "")
-# merged_data["Address"] = ""
+
+
 def add_address_to_equipment(equipment_id):
     """
     Function to add an address to each rows which match the given equipment. 
@@ -113,24 +49,11 @@ def add_address_to_equipment(equipment_id):
         merged_data.loc[row, "Address"] = address_from_latitude_longitude(latitude[row], longitude[row])
 
 
-
     
 for equipment in equip_id_list:
     add_address_to_equipment(equipment)
 
 # Saving to a new csv file
-merged_data.to_csv("testing.csv", index=False)
+merged_data.to_csv("results.csv", index=False)
 print("Data saved")
 
-
-#result_get_adress["formatted_address"]
-#r = requests.get("https://maps.google.com/maps/api/geocode/"
-
-# r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=" + key)
-# address = r.json()["results"][0]["formatted_address"]
-# print(address)
-
-
-    
-
-# print(address_from_latitude_longitude(40.714224, -73.961452))
